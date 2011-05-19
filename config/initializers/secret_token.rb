77517707905185cefabbe35d6a6bb32c85d6d579
Hilary L. Troiano <moonlight_anime@yahoo.com>
2011-05-19 18:37:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gargleblasted::Application.config.secret_token = 'f39d1558f368b4c6f31925aef70f807d9d2aa92fca2e709dfb18ef58e2fef0982c3f370b8831c3982d03e8b08a29704089449c5daf802ddbd8498b09b7f3db43'
